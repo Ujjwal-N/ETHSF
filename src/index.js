b5f0eed375c9825f8d53db936cc1f4e3cec5969e
Ujjwal Nadhani <ujjwalnadhnai@Ujjwals-MacBook-Pro.local>
2022-11-05 15:28:36 -0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,72 @@
+import Koa from 'koa';
+import Router from '@koa/router';
+
+import { isReady, PrivateKey, Field, Signature, Poseidon } from 'snarkyjs';
+
+const PORT = process.env.PORT || 3000;
+
+const app = new Koa();
+const router = new Router();
+
+const numValidators = 4;
+
+async function signData(ach) {
+  await isReady;
+  var outputString = 'ACH: ' + ach + '\n';
+
+  const privateKey = PrivateKey.fromBase58(
+    process.env.PRIVATE_KEY ??
+      'EKF65JKw9Q1XWLDZyZNGysBbYG21QbJf3a4xnEoZPZ28LKYGMw53'
+  );
+
+  const publicKey = privateKey.toPublicKey();
+
+  const subKeys = [];
+
+  for (var i = 1; i < numValidators + 1; i++) {
+    outputString += 'Validator ' + i + ': ';
+
+    const privKeyFields = PrivateKey.random().toFields();
+    privKeyFields.push(Field(ach));
+    const curKey = Poseidon.hash(privKeyFields);
+
+    subKeys.push(curKey);
+    outputString += curKey + '\n';
+
+    // const curSig = Signature.create(privateKey, [Field(i), curKey]);
+    // subSignatures.push(curSig);
+    // outputString += curSig + "\n";
+  }
+  outputString += 'Transferring subkeys to validators...\n';
+
+  const topLevelHash = Poseidon.hash(subKeys);
+  outputString += 'Top Level Hash: ' + topLevelHash;
+
+  console.log('here2');
+  const signature = Signature.create(privateKey, [Field(ach), topLevelHash]);
+
+  return {
+    data: { ach: ach, topLevelHash: topLevelHash },
+    signature: signature,
+    publicKey: publicKey,
+    display: outputString,
+  };
+}
+
+router.get('/ach/:ach', async (ctx) => {
+  console.log('here');
+  let signedData = await signData(ctx.params.ach);
+  ctx.body =
+    'Signature(r,s):' +
+    '(' +
+    signedData.signature.r.toString() +
+    ', ' +
+    signedData.signature.s.toJSON().toString() +
+    ')' +
+    '\n' +
+    signedData.display;
+});
+
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(PORT);
